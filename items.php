f393889842841f0a1be8784d9005bc337a65b420
Simpletextzar <simpletextzar@gmail.com>
2025-10-07 21:45:39 -0500

    items.php agregado

---
+++ items.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inventario</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <h2>Productos del inventario</h2>
+    <?php
+    $connection = pg_connect("host=aws-1-sa-east-1.pooler.supabase.com dbname=db_teejosh user=postgres.piearfkkossvytunnrfk password=patoloco090");
+    if(!$connection) {
+        echo "Error de conexion.<br>";
+        exit;
+    }
+
+    $result = pg_query($connection, "SELECT * FROM item");
+
+    ?>
+    <table>
+        <tr>
+            <th>ID</th>
+            <th>ID_producto</th>
+            <th>Precio</th>
+            <th>Cantidad</th>
+            <th>ID_edicion</th>
+            <th>ID_lenguaje</th>
+            <th>Fecha de ingreso</th>
+        </tr>
+
+        <?php
+        while($row = pg_fetch_assoc($result)) {
+            echo "
+            <tr>
+                <td>$row[id]</td>
+                <td>$row[id_producto]</td>
+                <td>$row[precio]</td>
+                <td>$row[cantidad]</td>
+                <td>$row[id_edicion]</td>
+                <td>$row[id_lenguaje]</td>
+                <td>$row[fecha_ingreso]</td>
+            </tr>
+            ";
+        }
+
+        ?>
+    </table>
+</body>
+</html>
