842a09e862162cf5e1d78f0907c17f9023796024
Simpletextzar <simpletextzar@gmail.com>
2025-10-07 22:04:09 -0500

    reset.css agregado

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Menú Principal - Inventario</title>
+    <link rel="stylesheet" href="reset.css">
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <h1>Menú Principal - Módulo de Inventario</h1>
+    <ul>
+        <li><a href="">Insertar a inventario</a></li>
+        <li><a href="items.php">Modificar inventario</a></li>
+        <li><a href="">Eliminar inventario</a></li>
+        <li><a href="">Mostrar inventario</a></li>
+    </ul>
+</body>
+</html>
