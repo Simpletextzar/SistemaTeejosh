d349a649113416f2b39680af313522fd480f72c1
Simpletextzar <simpletextzar@gmail.com>
2025-10-10 08:13:46 -0500

    conn to db nueva

--- db_connect.php
+++ db_connect.php
@@ -1,5 +1,5 @@
 <?php
-$connection = pg_connect("host=aws-1-sa-east-1.pooler.supabase.com dbname=db_teejosh user=postgres.piearfkkossvytunnrfk password=patoloco090");
+$connection = pg_connect("host=db.piearfkkossvytunnrfk.supabase.co dbname=postgres user=postgres password=patoloco090");
 if(!$connection) {
     echo "Error de conexion.<br>";
     exit;
